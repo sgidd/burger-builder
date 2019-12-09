94596a1c014723540d2d6b604e192f9dcc052ea6
sunil <sunilgidd051@gmail.com>
2019-12-10 00:44:24 +0530

    Reusing Backdrop to SideDrawer

--- src/components/Layout/Layout.js
+++ src/components/Layout/Layout.js
@@ -1,18 +1,32 @@
-import React from 'react';
+import React,{Component} from 'react';
 
 import Aux from '../../hoc/Auxillary';
 import layoutClasses from './Layout.css';
 import Toolbar from '../Navigation/Toolbar/Toolbar';
 import SideDrawer from '../Navigation/SideDrawer/SideDrawer';
 
-const layout = (props) => (
-    <Aux>
-        <Toolbar />
-        <SideDrawer />
-        <main className={layoutClasses.Content}>
-            {props.children}
-        </main>
-    </Aux>
-);
+class Layout extends Component{
+    state= {
+        showSideDrawer :true,
+    }
 
-export default layout;
+    sideDrawerClosedHandler = () => {
+        this.setState({showSideDrawer :false});
+    }
+    render(){
+        return (
+            <Aux>
+            <Toolbar />
+            <SideDrawer 
+             showSideDrawer = {this.state.showSideDrawer}
+             closed={this.sideDrawerClosedHandler}/>
+            <main className={layoutClasses.Content}>
+                {this.props.children}
+            </main>
+            </Aux>
+        );
+
+    }
+}
+
+export default Layout;
--- src/components/Navigation/SideDrawer/SideDrawer.js
+++ src/components/Navigation/SideDrawer/SideDrawer.js
@@ -1,11 +1,20 @@
 import React from 'react';
 import Logo from '../../Logo/Logo';
 import NavigationItems from '../NavigationItems/NavigationItems';
-import sideDrawerClasses from './SideDrawer.css'
+import sideDrawerClasses from './SideDrawer.css';
+import Backdrop from '../../UI/Backdrop/Backdrop';
+import Aux from '../../../hoc/Auxillary';
+
 const sideDrawer = (props) => {
-    //..
+    let attachedClasses = [sideDrawerClasses.SideDrawer, sideDrawerClasses.Close];
+
+    if(props.showSideDrawer){
+        attachedClasses = [sideDrawerClasses.SideDrawer, sideDrawerClasses.Open];
+    }
     return (
-        <div className={sideDrawerClasses.SideDrawer}>
+        <Aux>
+            <Backdrop showModal ={props.showSideDrawer} clicked={props.closed}/>
+            <div className={attachedClasses.join(' ')}>
             {/* <Logo height="11%"/> */} 
 
             <div className={sideDrawerClasses.Logo}>
@@ -14,7 +23,8 @@ const sideDrawer = (props) => {
             <nav>
                 <NavigationItems />
             </nav>
-        </div>
+            </div>
+        </Aux>
     );
 };
 
