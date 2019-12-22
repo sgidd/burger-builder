ce54243b1176f13ea6dfaa12e02ab7d7e4a57d51
sunil <sunilgidd051@gmail.com>
2019-12-22 23:43:05 +0530

    Accessing the server - creating axios instance , post request , Spinner while request

--- src/components/UI/Modal/Modal.js
+++ src/components/UI/Modal/Modal.js
@@ -11,7 +11,7 @@ class Modal extends Component {
     }
 
     shouldComponentUpdate(nextProps, nextState){
-        return nextProps.showModal !== this.props.showModal;
+        return nextProps.showModal !== this.props.showModal || nextProps.children !== this.props.children;
     }
 
     render() {
--- src/containers/BurgerBuilder/BurgerBuilder.js
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -6,6 +6,10 @@ import BuildControls from '../../components/Burger/BuildControls/BuildControls';
 
 import Modal from '../../components/UI/Modal/Modal';
 import OrderSummary from '../../components/Burger/OrderSummary/OrderSummary';
+import Spinner from '../../components/UI/Spinner/Spinner';
+import axios from '../../axios-orders';
+
+
 const INGREDIENT_PRICES = {
     salad : 0.5,
     cheese: 0.4,
@@ -28,6 +32,7 @@ class BurgerBuilder extends Component {
         totalPrice : 4,
         purchasable : false,
         purchasing: false,
+        loading:false
     }
 
     updatePurchaseState = (ingredients) => {
@@ -95,9 +100,9 @@ class BurgerBuilder extends Component {
     }
 
 
-    purchaseHandler (){
-        this.setState({purchasing : true})
-    } //error: Cannot read property 'setState' of undefined
+    // purchaseHandler (){
+    //     this.setState({purchasing : true})
+    // } //error: Cannot read property 'setState' of undefined
 
 
     purchaseHandler = () => {
@@ -109,7 +114,32 @@ class BurgerBuilder extends Component {
     }
 
     purchaseContinueHandler = () =>{
-        alert('You Continue!');
+        //alert('You Continue!');
+        this.setState({loading: true});
+        const order ={
+            ingredients : this.state.ingredients,
+            price : this.state.totalPrice,
+            customer: {
+                name : 'Sunil Gidd',
+                address: {
+                    street: 'FCI Road',
+                    zipcode: '560067',
+                    country: 'India'
+                },
+                email: 'sunilgidd051@gmail.com'
+            },
+            deliveryMethod: 'Fastest'
+        }
+
+        axios.post('/orders.json', order)
+            .then(response => {
+                //console.log(response);
+                this.setState({loading: false, purchasing: false});
+            })
+            .catch(error => {
+               // console.log(error);
+                this.setState({loading: false, purchasing:false});
+            })
     }
 
     render(){
@@ -122,15 +152,21 @@ class BurgerBuilder extends Component {
             disabledInfo[key] = disabledInfo[key] <= 0;
         } 
         //{salad: true, meat:false, ...}
+
+        let orderSummary = <OrderSummary 
+        ingredients={this.state.ingredients} 
+        purchaseCancelled = {this.purchaseCancelHandler}
+        purchaseContinued ={this.purchaseContinueHandler}
+        totalPrice={this.state.totalPrice}
+        />
+
+        if(this.state.loading){
+            orderSummary = <Spinner />
+        }
         return (
             <Aux>
                 <Modal showModal={this.state.purchasing} modalClosed={this.purchaseCancelHandler}>
-                    <OrderSummary 
-                    ingredients={this.state.ingredients} 
-                    purchaseCancelled = {this.purchaseCancelHandler}
-                    purchaseContinued ={this.purchaseContinueHandler}
-                    totalPrice={this.state.totalPrice}
-                    />
+                    {orderSummary}
                 </Modal>
                 <Burger ingredients = {this.state.ingredients}/>
               
