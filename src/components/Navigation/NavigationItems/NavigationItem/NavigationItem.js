2f5a7820396deb0b5656cef13788badfeb5fa4ff
sunil <sunilgidd051@gmail.com>
2019-12-29 19:26:52 +0530

    Implementing Navigation links in navigation folder using NavLink

--- src/components/Navigation/NavigationItems/NavigationItem/NavigationItem.js
+++ src/components/Navigation/NavigationItems/NavigationItem/NavigationItem.js
@@ -1,12 +1,22 @@
 import React from 'react';
 import classes from './NavigationItem.css'
 
+import { NavLink } from 'react-router-dom';
 
 const navigationItem = (props) => (
     <li className={classes.NavigationItem}>
-        <a 
+        {/* <a 
         href={props.link}
-        className={props.active ? classes.active : null}>{props.children}</a>
+        className={props.active ? classes.active : null}>{props.children}</a> */}
+
+        <NavLink
+        to={props.link}  
+        // to is treated as prefix here so active will apply on bith when clicked on orders so use exact keyword or props 
+        //exact //but this will add on all li so pass it from outside to just apply on "/" or one specific 
+        exact={props.exact}
+        activeClassName={classes.active}>
+            {props.children}
+        </NavLink>
     </li>
 );
 
