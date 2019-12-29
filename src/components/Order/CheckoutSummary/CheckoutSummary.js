9b82c339059a0e96d966f8dcde053d18192f64be
sunil <sunilgidd051@gmail.com>
2019-12-29 10:40:33 +0530

    Building and testing Checkout container to display burger preview and summary

---
+++ src/components/Order/CheckoutSummary/CheckoutSummary.js
@@ -0,0 +1,25 @@
+import React from 'react';
+import Burger from '../../Burger/Burger';
+import classes from './CheckoutSummary.css';
+import Button from '../../UI/Button/Button';
+
+const checkoutSummary = (props) => {
+    return(
+        <div className={classes.CheckoutSummary}>
+            <h1>We hope it tastes well!</h1>
+            <div style={{width: '100%', margin:'auto'}}>
+                <Burger ingredients={props.ingredients} />
+                
+                <Button 
+                btnType="Danger" 
+                btnClicked>CANCEL</Button>
+                
+                <Button 
+                btnType="Success" 
+                btnClicked>CONTINUE</Button>
+            </div>
+        </div>
+    );
+}
+
+export default checkoutSummary;
