094ba690560f289b42a0756fdb0876f2264f13e0
sunil <sunilgidd051@gmail.com>
2020-01-25 19:59:45 +0530

    Adding Dropdown inpu and handling user input

--- src/containers/Checkout/ContactData/ContactData.js
+++ src/containers/Checkout/ContactData/ContactData.js
@@ -86,6 +86,19 @@ class ContactData extends Component {
             })
 
     }
+
+    inputChangeHandler = (event , inputIdentifier) => {
+        // console.log(event.target.value);
+        const updatedForm = {
+            ...this.state.orderForm
+        };
+        const updatedFormElement  = {
+            ...updatedForm[inputIdentifier]
+        }
+        updatedFormElement.value = event.target.value;
+        updatedForm[inputIdentifier] = updatedFormElement;
+        this.setState({orderForm: updatedForm});
+    }
     render(){
         const formElementsArray = [];
         for(let key in this.state.orderForm){
@@ -103,7 +116,8 @@ class ContactData extends Component {
                     key={formElement.id}
                     elementType={formElement.config.elementType}
                     elementConfig={formElement.config.elementConfig}
-                    value= {formElement.config.value} />
+                    value= {formElement.config.value} 
+                    changed={ (event) => this.inputChangeHandler(event , formElement.id)}/>
                 ))
             }
             <Button btnType="Success" btnClicked={this.orderHandler}>ORDER</Button>
